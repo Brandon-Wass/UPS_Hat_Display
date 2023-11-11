72a270c4ce78cf99989b84bcc3f125e47b9e21f5
B-Boone <101531474+B-Boone@users.noreply.github.com>
2023-11-10 21:41:21 -0500

    refined the whole code
    Structure: Utilizes object-oriented programming. Encapsulates related functionalities within a class, making it modular and organized.
    Readability: Higher, due to the use of a class structure that logically groups related functionalities. The segment mappings are more concise and manageable.
    Maintainability: Easier to maintain and extend. For instance, adding a new display feature would likely involve adding a method to the class, which is more straightforward and less error-prone.
    Efficiency: Likely to be more efficient in execution due to reduced repetition in code and more streamlined segment control.
    Best Practices: Adheres better to modern programming principles, such as encapsulation and modularity.

--- X728_Client.py
+++ X728_Client.py
@@ -1,229 +1,119 @@
 import json
 import RPi.GPIO as GPIO
-import sys
 from threading import Thread, Event
 from time import sleep
 
-# Constants
-REFRESH_RATE = 0.001  # Refresh rate for display update
-BATTERY_FILE_PATH = 'battery.json'  # Path to read and parse battery.json file
+class LEDDisplay:
+    REFRESH_RATE = 0.001
+    BATTERY_FILE_PATH = 'battery.json'
 
-# Setup GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-pins = {
-    'Pin1': 17, 'Pin2': 27, 'Pin3': 22, 'Pin4': 23, 'Pin5': 24,
-}
+    def __init__(self):
+        self.pins = {
+            'Pin1': 17, 'Pin2': 27, 'Pin3': 22, 'Pin4': 23, 'Pin5': 24,
+        }
+        self.setup_gpio()
 
-# Define set_segment function to control the segments
-def set_segment(pin_high, pin_low):
-    # Set all pins to input (high-impedance) first
-    for pin in pins.values():
-        GPIO.setup(pin, GPIO.IN)
+        self.left_number = [0]
+        self.right_number = [0]
+        self.percent_symbol = ['off']
+        self.indicator_light = ['off']
+        self.stop_event = Event()
 
-    # Now, only set the two pins we're interested in to output
-    GPIO.setup(pin_high, GPIO.OUT)
-    GPIO.setup(pin_low, GPIO.OUT)
+        # Define segment control mappings for a multiplexed display
+        self.segment_mappings = {
+            'left_digit': {
+                0: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5']), (self.pins['Pin1'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin4'])],
+                1: [(self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin3'])],
+                2: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin1'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5'])],
+                3: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5'])],
+                4: [(self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin4'])],
+                5: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5']), (self.pins['Pin1'], self.pins['Pin4'])],
+                6: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin4'])],
+                7: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin3'])],
+                8: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5']), (self.pins['Pin1'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin4'])],
+                9: [(self.pins['Pin1'], self.pins['Pin2']), (self.pins['Pin2'], self.pins['Pin1']), (self.pins['Pin2'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin3']), (self.pins['Pin1'], self.pins['Pin5']), (self.pins['Pin1'], self.pins['Pin4'])]
+            },
+            'right_digit': {
+                0: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin3'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin2']), (self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin4'], self.pins['Pin3'])],
+                1: [(self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin4'], self.pins['Pin3'])],
+                2: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin5']), (self.pins['Pin3'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin2'])],
+                3: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin3']), (self.pins['Pin3'], self.pins['Pin2'])],
+                4: [(self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin3'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin4'], self.pins['Pin3'])],
+                5: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin3'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin3']), (self.pins['Pin3'], self.pins['Pin2'])],
+                6: [(self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin3'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin2']), (self.pins['Pin4'], self.pins['Pin3']), (self.pins['Pin3'], self.pins['Pin5'])],
+                7: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin4'], self.pins['Pin3'])],
+                8: [(self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin3'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin2']), (self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin4'], self.pins['Pin3']), (self.pins['Pin3'], self.pins['Pin5'])],
+                9: [(self.pins['Pin3'], self.pins['Pin4']), (self.pins['Pin2'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin1']), (self.pins['Pin3'], self.pins['Pin5']), (self.pins['Pin4'], self.pins['Pin3'])]
+            },
+            'percent_symbol': {
+                'on': [(self.pins['Pin4'], self.pins['Pin2'])],
+                'off': []
+            },
+            'indicator_light': {
+                'green': [(self.pins['Pin5'], self.pins['Pin3'])],
+                'orange': [(self.pins['Pin5'], self.pins['Pin3']), (self.pins['Pin5'], self.pins['Pin4'])],
+                'red': [(self.pins['Pin5'], self.pins['Pin4'])],
+                'off': []
+            }
+        }
 
-    # Drive the selected segment
-    GPIO.output(pin_high, GPIO.HIGH)
-    GPIO.output(pin_low, GPIO.LOW)
+    def setup_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        for pin in self.pins.values():
+            GPIO.setup(pin, GPIO.IN)
 
-# Define clear_segments function
-def clear_segments():
-    # This function now sets all pins to input mode to 'turn off' all segments.
-    for pin in pins.values():
-        GPIO.setup(pin, GPIO.IN)
+    def set_segment(self, pin_high, pin_low):
+        GPIO.setup(pin_high, GPIO.OUT)
+        GPIO.setup(pin_low, GPIO.OUT)
+        GPIO.output(pin_high, GPIO.HIGH)
+        GPIO.output(pin_low, GPIO.LOW)
 
-# Segment control functions for the left side
-def Left_Top():
-    set_segment(pins['Pin1'], pins['Pin2'])
+    def clear_segments(self):
+        for pin in self.pins.values():
+            GPIO.setup(pin, GPIO.IN)
 
-def Left_Top_Left():
-    set_segment(pins['Pin1'], pins['Pin4'])
+    def display_segment(self, segments):
+        for pin_high, pin_low in segments:
+            self.set_segment(pin_high, pin_low)
+            sleep(self.REFRESH_RATE)
 
-def Left_Top_Right():
-    set_segment(pins['Pin2'], pins['Pin1'])
+    def update_display(self):
+        while not self.stop_event.is_set():
+            self.clear_segments()
+            self.display_segment(self.segment_mappings['left_digit'][self.left_number[0]])
+            self.display_segment(self.segment_mappings['right_digit'][self.right_number[0]])
+            self.display_segment(self.segment_mappings['percent_symbol'][self.percent_symbol['off']])
+            self.display_segment(self.segment_mappings['indicator_light'][self.indicator_light['off']])
+            sleep(self.REFRESH_RATE)
 
-def Left_Middle():
-    set_segment(pins['Pin2'], pins['Pin4'])
+    def read_battery_file(self):
+        while not self.stop_event.is_set():
+            try:
+                with open(self.BATTERY_FILE_PATH, 'r') as file:
+                    data = json.load(file)
+                self.left_number[0] = data.get('left', 0)
+                self.right_number[0] = data.get('right', 0)
+                self.percent_symbol[0] = data.get('percent', 'off')
+                self.indicator_light[0] = data.get('indicator', 'off')
+            except Exception as e:
+                print(f"Error: {e}")
+            sleep(0.5)
 
-def Left_Bottom_Left():
-    set_segment(pins['Pin1'], pins['Pin3'])
+    def start(self):
+        Thread(target=self.update_display, daemon=True).start()
+        Thread(target=self.read_battery_file, daemon=True).start()
 
-def Left_Bottom_Right():
-    set_segment(pins['Pin2'], pins['Pin3'])
-
-def Left_Bottom():
-    set_segment(pins['Pin1'], pins['Pin5'])
-
-# Segment control functions for the right side
-def Right_Top():
-    set_segment(pins['Pin2'], pins['Pin5'])
-
-def Right_Top_Left():
-    set_segment(pins['Pin3'], pins['Pin4'])
-
-def Right_Top_Right():
-    set_segment(pins['Pin4'], pins['Pin3'])
-
-def Right_Middle():
-    set_segment(pins['Pin3'], pins['Pin5'])
-
-def Right_Bottom_Left():
-    set_segment(pins['Pin3'], pins['Pin1'])
-
-def Right_Bottom_Right():
-    set_segment(pins['Pin4'], pins['Pin1'])
-
-def Right_Bottom():
-    set_segment(pins['Pin3'], pins['Pin2'])
-
-def Percent_Symbol_On():
-    set_segment(pins['Pin4'], pins['Pin2'])
-
-def Percent_Symbol_Off():
-    GPIO.setup(pins['Pin4'], GPIO.IN)
-    GPIO.setup(pins['Pin2'], GPIO.IN)
-
-def Green_Light_On():
-    set_segment(pins['Pin5'], pins['Pin3'])
-
-def Orange_Light_On():
-    set_segment(pins['Pin5'], pins['Pin3'])
-    sleep(0.001)
-    set_segment(pins['Pin5'], pins['Pin4'])
-    sleep(0.009)
-
-def Red_Light_On():
-    set_segment(pins['Pin5'], pins['Pin4'])
-
-def Light_Off():
-    GPIO.setup(pins['Pin5'], GPIO.IN)
-    GPIO.setup(pins['Pin3'], GPIO.IN)
-    GPIO.setup(pins['Pin4'], GPIO.IN)
-
-# Define mapping for the segments of each number
-left_number_segments = {
-    0: [Left_Top, Left_Top_Left, Left_Bottom_Left, Left_Bottom, Left_Bottom_Right, Left_Top_Right],
-    1: [Left_Top_Right, Left_Bottom_Right],
-    2: [Left_Top, Left_Top_Right, Left_Middle, Left_Bottom_Left, Left_Bottom],
-    3: [Left_Top, Left_Top_Right, Left_Middle, Left_Bottom_Right, Left_Bottom],
-    4: [Left_Top_Left, Left_Middle, Left_Top_Right, Left_Bottom_Right],
-    5: [Left_Top, Left_Top_Left, Left_Middle, Left_Bottom_Right, Left_Bottom],
-    6: [Left_Top_Left, Left_Bottom_Left, Left_Bottom, Left_Bottom_Right, Left_Middle],
-    7: [Left_Top, Left_Top_Right, Left_Bottom_Right],
-    8: [Left_Top, Left_Top_Right, Left_Middle, Left_Bottom_Right, Left_Bottom, Left_Bottom_Left, Left_Top_Left],
-    9: [Left_Top_Left, Left_Top, Left_Top_Right, Left_Middle, Left_Bottom_Right]
-}
-
-right_number_segments = {
-    0: [Right_Top, Right_Top_Left, Right_Bottom_Left, Right_Bottom, Right_Bottom_Right, Right_Top_Right],
-    1: [Right_Top_Right, Right_Bottom_Right],
-    2: [Right_Top, Right_Top_Right, Right_Middle, Right_Bottom_Left, Right_Bottom],
-    3: [Right_Top, Right_Top_Right, Right_Middle, Right_Bottom_Right, Right_Bottom],
-    4: [Right_Top_Left, Right_Middle, Right_Top_Right, Right_Bottom_Right],
-    5: [Right_Top, Right_Top_Left, Right_Middle, Right_Bottom_Right, Right_Bottom],
-    6: [Right_Top_Left, Right_Bottom_Left, Right_Bottom, Right_Bottom_Right, Right_Middle],
-    7: [Right_Top, Right_Top_Right, Right_Bottom_Right],
-    8: [Right_Top, Right_Top_Right, Right_Middle, Right_Bottom_Right, Right_Bottom, Right_Bottom_Left, Right_Top_Left],
-    9: [Right_Top_Left, Right_Top, Right_Top_Right, Right_Middle, Right_Bottom_Right]
-}
-
-percent_symbol_segments = {
-    'on': [Percent_Symbol_On],
-    'off': [Percent_Symbol_Off]
-}
-
-indicator_light_segments = {
-    'green': [Green_Light_On],
-    'orange': [Orange_Light_On],
-    'red': [Red_Light_On],
-    'off': [Light_Off]
-}
-
-# Functions to display numbers, percent symbol, and light
-def display_number(number, segment_mapping):
-    for segment in segment_mapping.get(number, []):
-        segment()
-        sleep(REFRESH_RATE)
-
-def display_percent(symbol, segment_mapping):
-    for segment in segment_mapping.get(symbol, []):
-        segment()
-        sleep(REFRESH_RATE)
-
-def display_indicator(indicator, segment_mapping):
-    for segment in segment_mapping.get(indicator, []):
-        segment()
-        sleep(REFRESH_RATE)
-
-# This event will be used to stop the thread when the program is exiting
-stop_event = Event()
-
-# This function will run in a separate thread to update the display
-def update_display(left_number, right_number, percent_symbol, indicator_light):
-    while not stop_event.is_set():
-        clear_segments()
-        display_number(left_number[0], left_number_segments)
-        display_number(right_number[0], right_number_segments)
-        display_percent(percent_symbol[0], percent_symbol_segments)
-        display_indicator(indicator_light[0], indicator_light_segments)
-        sleep(REFRESH_RATE)
-
-# This function reads the battery.json file and updates the display values
-def read_battery_file(left_number, right_number, percent_symbol, indicator_light, file_path=BATTERY_FILE_PATH):
-    while not stop_event.is_set():
-        try:
-            with open(file_path, 'r') as file:
-                data = json.load(file)
-                # Log the data to verify its correctness
-                print(f"Data read from battery.json: {data}")
-
-                # Check if all keys are in the JSON data
-                if all(key in data for key in ['left', 'right', 'percent', 'indicator']):
-                    left_number[0] = data['left']
-                    right_number[0] = data['right']
-                    percent_symbol[0] = data['percent']
-                    indicator_light[0] = data['indicator']
-                else:
-                    print("Error: One or more keys are missing in the battery.json file.")
-        except IOError as e:
-            print(f"Error reading {file_path}: {e}")
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON from {file_path}: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-
-        sleep(0.5)  # Check for new data every second or adjust as needed
-
-# Main program loop
-def main():
-    # Initialize with default values
-    left_number = [0]
-    right_number = [0]
-    percent_symbol = ['off']
-    indicator_light = ['off']
-
-    # Start the display thread
-    display_thread = Thread(target=update_display, args=(left_number, right_number, percent_symbol, indicator_light), daemon=True)
-    display_thread.start()
-
-    # Start the battery file reading thread
-    battery_thread = Thread(target=read_battery_file, args=(left_number, right_number, percent_symbol, indicator_light), daemon=True)
-    battery_thread.start()
-
-    try:
-        # This loop now just waits for the threads to be stopped
-        while True:
-            sleep(1)  # Main thread does nothing, just sleeps
-    finally:
-        stop_event.set()  # Signal the threads to stop
-        display_thread.join()
-        battery_thread.join()
-        clear_segments()
+    def stop(self):
+        self.stop_event.set()
+        self.clear_segments()
         GPIO.cleanup()
-        print("\nCleanup complete. Program exited gracefully.")
 
 if __name__ == '__main__':
-    main()
+    display = LEDDisplay()
+    try:
+        display.start()
+        while True:
+            sleep(1)
+    finally:
+        display.stop()

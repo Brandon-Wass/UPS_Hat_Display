9af27c5b20a94daa906adb95bfc62c607518adf9
B-Boone <101531474+B-Boone@users.noreply.github.com>
2023-11-10 22:30:19 -0500

    Rewrote the code
    Updated code to use OOP, and changed indicator values

--- X728_Server.py
+++ X728_Server.py
@@ -8,53 +8,47 @@ import struct
 import sys
 from time import sleep
 
-##########
-
-BATTERY_FILE_PATH = 'battery.json'  # Path to write to battery.json file
+BATTERY_FILE_PATH = 'battery.json'
 CHARGING_STATUS_PORT = 6
-I2C_ADDR=0x36 # Address of the I2C battery fuel gauging chip on x728 V-2.3 UPS hat
-bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+I2C_ADDR = 0x36
 
-##########
+class BatteryMonitor:
+    def __init__(self, bus_number=1, address=0x36):
+        self.bus = smbus.SMBus(bus_number)
+        self.address = address
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(CHARGING_STATUS_PORT, GPIO.IN)
+    def readVoltage(self):
+        try:
+            read = self.bus.read_word_data(self.address, 2)
+            swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+            return swapped * 1.25 / 1000 / 16
+        except Exception as e:
+            logging.error("Error reading voltage: %s", e)
+            return None
 
-##########
-
-# Function to read voltage from the fuel gauge
-def readVoltage(bus):
-    address = I2C_ADDR
-    read = bus.read_word_data(address, 2)
-    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-    voltage = swapped * 1.25 /1000/16
-    return voltage
-
-# Function to read capacity from the fuel gauge
-def readCapacity(bus):
-    address = I2C_ADDR
-    read = bus.read_word_data(address, 4)
-    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-    capacity = swapped / 256
-    if capacity > 99:
-        capacity = 99
-    return int(capacity)
+    def readCapacity(self):
+        try:
+            read = self.bus.read_word_data(self.address, 4)
+            swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+            capacity = min(swapped / 256, 99)
+            return int(capacity)
+        except Exception as e:
+            logging.error("Error reading capacity: %s", e)
+            return None
 
 # Function to prepare capacity reading for display
-def prepare_readCapacity(bus):
-    capacity = readCapacity(bus)
+def prepare_readCapacity(battery_monitor):
+    capacity = battery_monitor.readCapacity()
     left_number = capacity // 10
     right_number = capacity % 10
     return left_number, right_number
 
 # Function to prepare voltage reading for display
-def prepare_readVoltage(bus):
-    voltage = readVoltage(bus)
-    if voltage > 3.80:
+def prepare_readVoltage(battery_monitor):
+    voltage = battery_monitor.readVoltage()
+    if voltage > 3.70:
         return 'green'
-    elif 3.40 <= voltage <= 3.80:
+    elif 3.40 <= voltage <= 3.70:
         return 'orange'
     elif voltage < 3.40:
         return 'red'
@@ -78,20 +72,28 @@ def send_status(left_number, right_number, percent_symbol, indicator_light):
 ##########
 
 def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(CHARGING_STATUS_PORT, GPIO.IN)
+
+    battery_monitor = BatteryMonitor()
+
     try:
         while True:
             try:
-                left_number, right_number = prepare_readCapacity(bus)
+                left_number, right_number = prepare_readCapacity(battery_monitor)
                 percent_symbol = check_charge()
-                indicator_light = prepare_readVoltage(bus)
+                indicator_light = prepare_readVoltage(battery_monitor)
                 send_status(left_number, right_number, percent_symbol, indicator_light)
-                sleep(5)  # Read and send new data to battery.json file every 5 seconds
+                sleep(5)
             except Exception as e:
-                logging.error("An error occurred: %s", e)
-                sleep(1)  # Wait a bit before retrying to prevent a spam of error messages
+                logging.error("An error occurred in the main loop: %s", e)
+                sleep(1)
     finally:
         GPIO.cleanup()
         logging.info("GPIO cleanup complete.")
 
 if __name__ == '__main__':
     main()
+
